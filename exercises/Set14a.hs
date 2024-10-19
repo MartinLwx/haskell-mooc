b61cd4268efcc68ed43fcdbbbfcc7e348171b45e
MartinLwx <MartinLwx@163.com>
2024-10-19 11:39:55 +0800

    Finish Exercise 14.5

--- exercises/Set14a.hs
+++ exercises/Set14a.hs
@@ -3,17 +3,17 @@ module Set14a where
 -- Remember to browse the docs of the Data.Text and Data.ByteString
 -- libraries while working on the exercises!
 
-import Mooc.Todo
-
 import Data.Bits
-import Data.Char
-import Data.Text.Encoding
-import Data.Word
-import Data.Int
-import qualified Data.Text as T
-import qualified Data.Text.Lazy as TL
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
+import Data.Char
+import Data.Int
+import qualified Data.Text as T
+import Data.Text.Encoding
+import qualified Data.Text.Lazy as TL
+import Data.Word
+import qualified Language.Haskell.TH.Ppr as T
+import Mooc.Todo
 
 ------------------------------------------------------------------------------
 -- Ex 1: Greet a person. Given the name of a person as a Text, return
@@ -28,7 +28,10 @@ import qualified Data.ByteString.Lazy as BL
 --  greetText (T.pack "Benedict Cumberbatch") ==> "Hello, Benedict Cumber...!"
 
 greetText :: T.Text -> T.Text
-greetText = todo
+greetText name =
+  if T.length name > 15
+    then T.pack "Hello, " <> T.take 15 name <> T.pack "...!"
+    else T.pack "Hello, " <> name <> T.pack "!"
 
 ------------------------------------------------------------------------------
 -- Ex 2: Capitalize every second word of a Text.
@@ -39,8 +42,13 @@ greetText = todo
 --   shout (T.pack "word")
 --     ==> "WORD"
 
+-- shout s =
+--   let words = T.words s
+--    in T.unwords $ map (\(s, idx) -> if idx `mod` 2 == 0 then s else T.toUpper s) (zip words [1 ..])
 shout :: T.Text -> T.Text
-shout = todo
+shout s =
+  let words = T.words s
+   in T.unwords $ zipWith ($) (cycle [T.toUpper, id]) words
 
 ------------------------------------------------------------------------------
 -- Ex 3: Find the longest sequence of a single character repeating in
@@ -51,7 +59,12 @@ shout = todo
 --   longestRepeat (T.pack "aabbbbccc") ==> 4
 
 longestRepeat :: T.Text -> Int
-longestRepeat = todo
+longestRepeat s = case T.uncons s of
+  Nothing -> 0
+  Just (x, xs) ->
+    let suffix = T.dropWhile (== x) xs
+     in let rhs = longestRepeat suffix
+         in max (T.length xs - T.length suffix + 1) rhs
 
 ------------------------------------------------------------------------------
 -- Ex 4: Given a lazy (potentially infinite) Text, extract the first n
@@ -64,7 +77,7 @@ longestRepeat = todo
 --   takeStrict 15 (TL.pack (cycle "asdf"))  ==>  "asdfasdfasdfasd"
 
 takeStrict :: Int64 -> TL.Text -> T.Text
-takeStrict = todo
+takeStrict n s = TL.toStrict $ TL.take n s
 
 ------------------------------------------------------------------------------
 -- Ex 5: Find the difference between the largest and smallest byte
@@ -76,7 +89,12 @@ takeStrict = todo
 --   byteRange (B.pack [3]) ==> 0
 
 byteRange :: B.ByteString -> Word8
-byteRange = todo
+byteRange s =
+  if B.length s == 0
+    then 0
+    else
+      let w8s = B.unpack s
+       in maximum w8s - minimum w8s
 
 ------------------------------------------------------------------------------
 -- Ex 6: Compute the XOR checksum of a ByteString. The XOR checksum of
@@ -97,7 +115,7 @@ byteRange = todo
 --   xorChecksum (B.pack []) ==> 0
 
 xorChecksum :: B.ByteString -> Word8
-xorChecksum = todo
+xorChecksum s = if B.length s == 0 then 0 else B.foldr1 xor s
 
 ------------------------------------------------------------------------------
 -- Ex 7: Given a ByteString, compute how many UTF-8 characters it
@@ -114,7 +132,9 @@ xorChecksum = todo
 --   countUtf8Chars (B.drop 1 (encodeUtf8 (T.pack "åäö"))) ==> Nothing
 
 countUtf8Chars :: B.ByteString -> Maybe Int
-countUtf8Chars = todo
+countUtf8Chars s = case decodeUtf8' s of
+  Left _ -> Nothing
+  Right t -> Just $ T.length t
 
 ------------------------------------------------------------------------------
 -- Ex 8: Given a (nonempty) strict ByteString b, generate an infinite
@@ -126,5 +146,4 @@ countUtf8Chars = todo
 --     ==> [0,1,2,2,1,0,0,1,2,2,1,0,0,1,2,2,1,0,0,1]
 
 pingpong :: B.ByteString -> BL.ByteString
-pingpong = todo
-
+pingpong s = BL.cycle $ BL.concat [BL.fromStrict s, BL.fromStrict (B.reverse s)]
--- exercises/Set14b.hs
+++ exercises/Set14b.hs
@@ -10,23 +10,21 @@ module Set14b where
 --
 -- Let's start with some imports:
 
-import Mooc.Todo
-
 -- Utilities
 import qualified Data.ByteString.Lazy as LB
 import Data.Maybe
 import qualified Data.Text as T
-import qualified Data.Text.Read as TR
 import Data.Text.Encoding (encodeUtf8)
-import Text.Read (readMaybe)
-
+import qualified Data.Text.Read as TR
 -- HTTP server
-import Network.Wai (pathInfo, responseLBS, Application)
-import Network.Wai.Handler.Warp (run)
-import Network.HTTP.Types (status200)
 
 -- Database
-import Database.SQLite.Simple (open,execute,execute_,query,query_,Connection,Query(..))
+import Database.SQLite.Simple (Connection, Query (..), execute, execute_, open, query, query_)
+import Mooc.Todo
+import Network.HTTP.Types (status200)
+import Network.Wai (Application, pathInfo, responseLBS)
+import Network.Wai.Handler.Warp (run)
+import Text.Read (readMaybe)
 
 ------------------------------------------------------------------------------
 -- Ex 1: Let's start with implementing some database operations. The
@@ -39,9 +37,13 @@ import Database.SQLite.Simple (open,execute,execute_,query,query_,Connection,Que
 -- these.
 --
 -- Below, you'll find three queries:
+
 -- * initQuery creates the database
+
 -- * depositQuery adds an (account, amount) row into the database
+
 -- * getAllQuery gets all (account, amount) pairs from the database.
+
 --   getAllQuery isn't needed for the implementation, but you can use it
 --   to test your answer.
 --
@@ -60,7 +62,6 @@ import Database.SQLite.Simple (open,execute,execute_,query,query_,Connection,Que
 --   Set14b> query_ db getAllQuery :: IO [(String,Int)]
 --   [("xxx",13),("yyy",5),("xxx",7)]
 
-
 initQuery :: Query
 initQuery = Query (T.pack "CREATE TABLE IF NOT EXISTS events (account TEXT NOT NULL, amount NUMBER NOT NULL);")
 
@@ -76,12 +77,15 @@ getAllQuery = Query (T.pack "SELECT account, amount FROM events;")
 -- NOTE! Do not add anything to the name, otherwise you'll get weird
 -- test failures later.
 openDatabase :: String -> IO Connection
-openDatabase = todo
+openDatabase filename = do
+  db <- open filename
+  execute_ db initQuery
+  return db
 
 -- given a db connection, an account name, and an amount, deposit
 -- should add an (account, amount) row into the database
 deposit :: Connection -> T.Text -> Int -> IO ()
-deposit = todo
+deposit db account amount = execute db depositQuery (account, amount)
 
 ------------------------------------------------------------------------------
 -- Ex 2: Fetching an account's balance. Below you'll find
@@ -112,7 +116,9 @@ balanceQuery :: Query
 balanceQuery = Query (T.pack "SELECT amount FROM events WHERE account = ?;")
 
 balance :: Connection -> T.Text -> IO Int
-balance = todo
+balance db account = do
+  res <- query db balanceQuery [account] :: IO [[Int]]
+  return $ sum $ map sum res
 
 ------------------------------------------------------------------------------
 -- Ex 3: Now that we have the database part covered, let's think about
@@ -120,8 +126,11 @@ balance = todo
 -- users can issue: Deposit and Balance.
 --
 -- The HTTP API will use paths like the following:
+
 -- * /deposit/smith/3 will deposit 3 into the account "smith"
+
 -- * /balance/lopez will query the balance of the account "lopez"
+
 --
 -- Your task is to implement the function parseCommand that takes the
 -- pathInfo (remember: a list of Texts) of a request, and returns the
@@ -151,7 +160,24 @@ parseInt :: T.Text -> Maybe Int
 parseInt = readMaybe . T.unpack
 
 parseCommand :: [T.Text] -> Maybe Command
-parseCommand = todo
+parseCommand [] = Nothing
+parseCommand (x : xs)
+  | x == T.pack "deposit" && length xs == 2 = case parseInt (last xs) of
+      Nothing -> Nothing
+      Just v -> Just $ Deposit (head xs) v
+  | x == T.pack "withdraw" && length xs == 2 = case parseInt (last xs) of
+      Nothing -> Nothing
+      Just v -> Just $ Deposit (head xs) (-v)
+  | x == T.pack "balance" && length xs == 1 = Just $ Balance (head xs)
+  | otherwise = Nothing
+
+-- then do
+--   v <- parseInt (last xs)
+--   let v1 = if x == T.pack "deposit" then v else -v
+-- else
+--   if x == T.pack "balance"
+--     then Just $ Balance (head xs)
+--     else Nothing
 
 ------------------------------------------------------------------------------
 -- Ex 4: Running commands. Implement the IO operation perform that takes a
@@ -177,7 +203,13 @@ parseCommand = todo
 --   "0"
 
 perform :: Connection -> Maybe Command -> IO T.Text
-perform = todo
+perform db Nothing = return $ T.pack "ERROR"
+perform db (Just (Deposit account amount)) = do
+  deposit db account amount
+  return $ T.pack "OK"
+perform db (Just (Balance account)) = do
+  res <- balance db account
+  return $ T.pack (show res)
 
 ------------------------------------------------------------------------------
 -- Ex 5: Next up, let's set up a simple HTTP server. Implement a WAI
@@ -197,7 +229,8 @@ encodeResponse t = LB.fromStrict (encodeUtf8 t)
 -- Remember:
 -- type Application = Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived
 simpleServer :: Application
-simpleServer request respond = todo
+simpleServer request respond = do
+  respond $ responseLBS status200 [] (encodeResponse (T.pack "BANK"))
 
 ------------------------------------------------------------------------------
 -- Ex 6: Now we finally have all the pieces we need to actually
@@ -226,7 +259,10 @@ simpleServer request respond = todo
 -- Remember:
 -- type Application = Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived
 server :: Connection -> Application
-server db request respond = todo
+server db request respond = do
+  let args = pathInfo request
+  res <- perform db (parseCommand args)
+  respond $ responseLBS status200 [] (encodeResponse res)
 
 port :: Int
 port = 3421
@@ -257,6 +293,7 @@ main = do
 --   - Open <http://localhost:3421/balance/simon> in your browser.
 --     You should see the text 11.
 
+-- see parseCommand
 
 ------------------------------------------------------------------------------
 -- Ex 8: Error handling. Modify the parseCommand function so that it
@@ -276,5 +313,3 @@ main = do
 --    - http://localhost:3421/deposit/pekka/1/3
 --    - http://localhost:3421/balance
 --    - http://localhost:3421/balance/matti/pekka
-
-
